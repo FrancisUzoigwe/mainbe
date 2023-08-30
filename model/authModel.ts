9a1357454afe6ce8659aacb3986bd94c9027db45
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2023-08-30 23:44:28 +0100

    testing

--- model/authModel.ts
+++ model/authModel.ts
@@ -1,1 +1,26 @@
 import mongoose from "mongoose";
+interface iAuth {
+  name?: string;
+  email?: string;
+  password?: string;
+  address?: string;
+  avatar?: string;
+  avatarID?: string;
+}
+
+export interface iAuthData extends iAuth, mongoose.Document {}
+const authModel = new mongoose.Schema(
+  {
+    name: { type: String },
+    email: { type: String, unique: true },
+    password: { type: String },
+    address: { type: String },
+    avatar: { type: String },
+    avatarID: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<iAuthData>("auths", authModel)
